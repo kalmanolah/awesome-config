4da6b4011ac86e92117ee4db1874e46be15c55bf
Kalman Olah <kalman@inuits.eu>
2014-04-14 13:39:16 +0200

    it seems nm-applet starts automatically now

--- rc.lua
+++ rc.lua
@@ -34,7 +34,7 @@ vars.autorun    = {
     vars.home_dir .. "bin/notify-listener.py &",
     --vars.home_dir .. ".dropbox-dist/dropboxd &",
     --"eval `gnome-keyring-daemon`",
-    "nm-applet &",
+    --"nm-applet &",
     --"bluetooth-applet &",
     "mpd && ncmpcpp pause", -- Start mpd and pause it immediately
     --"compton -cCGb -l -10 -t -10 -r 10 -o 0.4"
