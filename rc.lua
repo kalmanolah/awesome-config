52fd9e5babb5cf352709cb9cd40822f25861e3f8
Kalman Olah <kalman@inuits.eu>
2014-11-08 21:48:33 +0100

    I don't really like LUA
    Signed-off-by: Kalman Olah <kalman@inuits.eu>

--- rc.lua
+++ rc.lua
@@ -606,7 +606,17 @@ clientbuttons = awful.util.table.join(
 )
 
 -- Generate and add the 'run or raise' key bindings to the globalkeys table
-globalkeys = awful.util.table.join(globalkeys, ror.genkeys(vars.modkey))
+globalkeys = awful.util.table.join(globalkeys, ror.genkeys(vars.modkey, {
+    ["f"]={"iceweasel", "Iceweasel"},
+    --["t"]={"thunderbird", "Thunderbird"},
+    ["t"]={"icedove","Icedove"},
+    ["l"]={"spotify", "Spotify"},
+    ["s"]={"skype", "Skype", "name"},
+    ["i"]={"urxvt -name irssi -e irssi", "irssi", "instance"},
+    ["m"]={"urxvt -name ncmpcpp -e ncmpcpp", "ncmpcpp", "instance"},
+    ["n"]={"nautilus", "Nautilus"},
+    ["e"]={"/opt/sublime_text/sublime_text", "sublime text"}
+}))
 
 -- Set keys
 root.keys(globalkeys)
