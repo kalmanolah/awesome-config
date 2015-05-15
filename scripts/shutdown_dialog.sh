1980668c97442e7440d837392d4e4f0eae92fb74
Kalman Olah <kalman@inuits.eu>
2015-05-15 02:25:05 +0200

    Modify shutdown script to use systemctl commands.
    Signed-off-by: Kalman Olah <kalman@inuits.eu>

--- scripts/shutdown_dialog.sh
+++ scripts/shutdown_dialog.sh
@@ -6,22 +6,23 @@ ACTION=$(zenity --width=150 --height=300 --list --radiolist --text="Select logou
 if [ -n "${ACTION}" ];then
   case $ACTION in
   Shutdown)
-    zenity --question --text "Are you sure you want to halt?" && gksudo halt
+    zenity --question --text "Are you sure you want to halt?" && systemctl poweroff
     ## or via ConsoleKit
     # dbus-send --system --dest=org.freedesktop.ConsoleKit.Manager \
     # /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
     ;;
   Reboot)
-    zenity --question --text "Are you sure you want to reboot?" && gksudo reboot
+    zenity --question --text "Are you sure you want to reboot?" && systemctl reboot
     ## Or via ConsoleKit
     # dbus-send --system --dest=org.freedesktop.ConsoleKit.Manager \
     # /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart
     ;;
   Suspend)
+    systemctl suspend
     #gksudo pm-suspend
-    dbus-send --system --print-reply --dest=org.freedesktop.Hal \
-    /org/freedesktop/Hal/devices/computer \
-    org.freedesktop.Hal.Device.SystemPowerManagement.Suspend int32:0
+    # dbus-send --system --print-reply --dest=org.freedesktop.Hal \
+    # /org/freedesktop/Hal/devices/computer \
+    # org.freedesktop.Hal.Device.SystemPowerManagement.Suspend int32:0
     # HAL is deprecated in newer systems in favor of UPower etc.
     # dbus-send --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend
     ;;
